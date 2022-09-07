c0aa7450b7154752efde3fcbd8dbec215ecced48
Clark19 <clarkgable1@gmail.com>
2022-09-08 03:39:58 +0900

    add: 8일차 과제 구현. Docker-compose / Mongodb  - docker-compose 를 이용해 express와 mongodb 연결후  1) 휴대폰 인증 토큰 발급 API (/tokens/phone Post), 토큰 도커 안 몽고디비에 저장  2) 인증번호 확인 api (Patch /tokens/phone) - 토큰 확인후 인증 플래그 업데이트

--- homework/day08/src/index.js
+++ homework/day08/src/index.js
@@ -1,13 +1,9 @@
-// const express = require('express')
 import express from "express";
 import { checkPhone, getToken, sendToToken } from "./phone.js";
 import swaggerUi from "swagger-ui-express";
-// import swaggerDocument from './swagger.json'; // 이걸 만들어 주는게 jsdoc 패키지 임
 import swaggerJsdoc from "swagger-jsdoc";
 import { options } from "./swagger/config.js";
 import cors from "cors";
-import ddd from "./phone.js";
-ddd();
 
 import {
   checkEmail,
@@ -15,65 +11,32 @@ import {
   sendTemplateToEmail,
 } from "./email.js";
 
-// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// dotenv.config()
 import "dotenv/config";
 import mongoose from "mongoose";
 import { Board } from "./models/board.model.js";
-
-// // swagger-jsdoc 용 옵션
-// const options = {
-//   failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: '나만의 미니 프로젝트 API 명세서',
-//       version: '1.0.0',
-//     },
-//   },
-//   apis: ['./swagger/*.swagger.js'], // 내가 따로 관리할 파일의 경로 입력하면 됨
-// };
-
-// const openapiSpecification = swaggerJsdoc(options);
+import { Token } from "./models/token.model.js";
 
 const app = express();
-// app.use(cors()) // 모든 (origin) 접속(자원 공유) 허용
-// 특정 오리진만(자원 요청 출발지) 하용시 배열에 넣음, 하나만 허용신 그냥 문잘열로 넣으면 됨
-// app.use(cors({
-//   origin: ["http://localhost:5500", "http://127.0.0.1:5500"]
-// }))
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 
 app.get("/", function (req, res) {
-  console.log("////");
-  res.send("Hello World113");
+  console.log("/ ping");
+  res.send("Hello World");
 });
 
 app.get("/boards", async function (req, res) {
-  console.log("/boards call");
-
   //1. 데이터 조회하는 로직 => DB에 접속해서 데이터 꺼내오기
-  // const result = [
-  //   { number: 1, writer: "name1", title: "title1", contents: "content1" },
-  //   { number: 2, writer: "name2", title: "title2", contents: "content2" },
-  //   { number: 3, writer: "name3", title: "title3", contents: "content3" },
-  // ];
-
   const result = await Board.find();
-
-  console.log("/boards end");
-
   // 2. db에서 꺼내온 결과를 브라우저에 응답(response) 주기
   res.send(result);
 });
 
 app.post("/boards", async function (req, res) {
   // 1.클라이언트에서 보낸 데이터 확인
-  console.log("/posts ...");
+  console.log("/boards post...");
   console.log(req.body);
 
   // 2. 데이터 등록하는 로직 => 디비에 접속해서 데이터 저장하기
@@ -91,21 +54,64 @@ app.post("/boards", async function (req, res) {
   res.send("게시물 등록에 성공했습니다."); // .send(200)
 });
 
-app.post("/tokens/phone", (req, res) => {
+app.post("/tokens/phone", async (req, res) => {
   // 1. 휴대폰 번호 자릿수 맞는지 확인하기
   console.log(req.body);
-  const phoneNumber = req.body.phoneNumber;
-  if (!checkPhone(phoneNumber)) return;
+  const phone = req.body.phone;
+  if (!checkPhone(phone)) return;
 
   // 2. 핸드폰 토큰 6자리 만들기
-  const mytoken = getToken();
-  if (!mytoken) return;
+  const token = getToken();
+  if (!token) return;
 
+  let isAuth = false;
+  const tokenDoc = new Token({
+    phone,
+    token,
+    isAuth,
+  });
+
+  let result = await Token.find({ phone: phone })
+  let result2;
+  console.log(result);
+  if (result.length === 0) {
+    result2 = await tokenDoc.save();
+    console.log("첨 생성 result2:", result2);
+  } else {
+    result2 = await Token.updateOne({phone: phone}, {$set: {token: token}})
+    console.log("업데이트 result2:", result2);
+
+  }
   // 3. 핸드폰 번호에 토큰 전송하기(추후: 그전엔 콘솔에 출력)
-  sendToToken(phoneNumber, mytoken);
+  sendToToken(phone, token);
 
-  res.send("인증완료!!!");
+  res.send(`${phone.slice(0,3)}-${phone.slice(3,7)}-${phone.slice(7)}번호로 인증번호 ${"991153"}를 전송합니다!!!`);
 });
+
+app.patch("/tokens/phone", async (req, res) => {
+  console.log("pathch:", req.body)
+  const { phone, token } = req.body
+  const result = await Token.find({ phone: phone })
+  console.log(result);
+  if (result.length === 0) {// 핸드폰 번호가 저장되어 있지 않다면 클라이언트에 false를 응답
+    res.send(false)
+    return
+  } else { // 핸드폰 번호가 저장되어 있을때
+    if (result[0].token !== token) { // 토큰이 다를때
+      res.send(false)
+      return
+    }
+    let result2 = await Token.updateOne({phone: phone}, {$set: {isAuth: true}})
+    console.log("업데이트 result2:", result2);
+    if (result2.modifiedCount > 0 || result2.matchedCount > 0) {
+      res.send(true)
+      return
+    } else {
+      res.send(false)
+      return
+    }
+  }
+})
 
 app.post("/users", (req, res) => {
   const { name, age, school, email } = req.body;
@@ -122,6 +128,7 @@ app.post("/users", (req, res) => {
 
   res.send("가입 완료");
 });
+
 
 // 0. 몽고DB 접속!!
 // database(mydocker04) 없으면 만든 후 접속 함.
