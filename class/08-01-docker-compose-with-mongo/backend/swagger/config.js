9014fec5fd8a7526f9e918e86dea268f1bdf7428
Clark19 <clarkgable1@gmail.com>
2022-09-07 23:59:31 +0900

    add: 8일차 Docker-compose / Mongodb 실습 내용 추가  - docker-compose 를 이용한 mongodb 연결  - mongoose(ODM): define model, get/post 실습

---
+++ class/08-01-docker-compose-with-mongo/backend/swagger/config.js
@@ -0,0 +1,15 @@
+// swagger-jsdoc 용 옵션
+export const options = {
+    failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: '나만의 미니 프로젝트 API 명세서',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./swagger/*.swagger.js'], // 내가 따로 관리할 파일의 경로 입력하면 됨
+  };
+  
+  // const openapiSpecification = swaggerJsdoc(options);
+  
