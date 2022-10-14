5be4159e9f4702994dcebbc613ebdc456eda8b48
Clark19 <clarkgable1@gmail.com>
2022-10-14 14:13:40 +0900

    feat: 29일차 실습 - ELK Stack

---
+++ class/29-01-elasticsearch/backend/src/apis/pointsTransactions/entities/pointTransaction.entity.ts
@@ -0,0 +1,48 @@
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum POINT_TRANSACTION_STATUS_ENUM {
+  PAYMENT = 'PAYMENT',
+  CANCEL = 'CANCEL',
+}
+
+// enum타입을 graphql에 등록
+registerEnumType(POINT_TRANSACTION_STATUS_ENUM, {
+  name: 'POINT_TRANSACTION_STATUS_ENUM',
+});
+
+@Entity()
+@ObjectType()
+export class PointTransaction {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column() // unique 하지만 안되는 이유는 PointTransaction 테이블을 업데이트하지 않고, 인서트 온리로 사용하여 모든 히스토리 기록하기 때문 uuid가 여러 row에 중복해서 나타날수 있기 때문에ㅣㄴ.
+  @Field(() => String)
+  impUid: string;
+
+  @Column()
+  @Field(() => Int)
+  amount: number;
+
+  // typform에서 enum 사용하는 법
+  @Column({ type: 'enum', enum: POINT_TRANSACTION_STATUS_ENUM }) // 일단 PAYMENT, CANCEL로 Enum 으로만 구분
+  @Field(() => POINT_TRANSACTION_STATUS_ENUM)
+  status: string; // 가상 계좌일 경우 'ready', 'paid', 'cancelled' 이런 상태 필요
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+
+  @CreateDateColumn()
+  @Field(() => String)
+  createdAt: Date;
+}
---
+++ class/29-01-elasticsearch/backend/src/apis/products/products.resolver.ts
@@ -0,0 +1,111 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateProductInput } from './dto/createProduct.input';
+import { UpdateProductInput } from './dto/updateProduct.input';
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(
+    private readonly productsService: ProductsService, //
+    private readonly elasticsearchService: ElasticsearchService,
+  ) {}
+
+  @Query(() => [Product]) // import 주의! @nestjs/graphql 임포트해야함.
+  async fetchProducts() {
+    /* 
+      GET _search
+      {
+        "query": {
+          "match_all": {}
+        }
+      }
+    */
+    // 엘라스틱서치에서 조회하기 연습
+    // const result = await this.elasticsearchService.search({
+    //   index: 'myproduct09', // 컬렉션 명
+    //   query: {
+    //     match_all: {}, // writer: "철수"
+    //   },
+    // });
+    // console.log('ES:', JSON.stringify(result)); //
+    // return '엘라스틱서치에서 조회 완료!!';
+    const result = await this.elasticsearchService.search({
+      index: 'myproduct09',
+      query: {
+        match_all: {},
+      },
+    });
+
+    console.log(JSON.stringify(result, null, ' '));
+    return '엘라스틱서치에서 조회 완료!!';
+
+    // 엘라스틱서치에서 조회해보기 위해 임시로 주석
+    // return this.productsService.findAll();
+  }
+
+  @Query(() => Product)
+  fetchProduct(
+    @Args('productId') productId: string, //
+  ) {
+    return this.productsService.findOne({ productId });
+  }
+
+  // @Mutation(() => Product)
+  @Mutation(() => String)
+  async createProduct(
+    @Args('createProductInput') createProductInput: CreateProductInput, // 이렇게 하면 아래처럼 하나하나 입력하지 않아도 됨._
+  ) {
+    // 엘라스틱서치에서 등록하기 연습
+    console.log(1121);
+    // this.elasticsearchService.create({
+    //   id: 'myid',
+    //   index: 'myproduct09', // 컬렉션 명
+    //   document: {
+    //     name: '철수',
+    //     age: 33,
+    //     school: '다람쥐초등학교',
+    //     ...createProductInput,
+    //   },
+    // });
+    this.elasticsearchService.create({
+      id: 'myid2',
+      index: 'myproduct09',
+      document: {
+        name: '철수',
+        age: 13,
+        school: '다람쥐초등학교',
+        ...createProductInput,
+      },
+    });
+
+    //  엘라스틱서치에 등록 해보기 위해 임시로 주석!!
+    // return this.productsService.create({ createProductInput });
+  }
+
+  // 수정과, 삭제시는 민감하게 주의 해야 함.
+  // 예를 들어 판매 완료후 가격 수정, 경매 완료후 가격 수정 등등
+  // 따라서 이게 삭제,수정이 되는지 확인/검증 후 막야 함.
+  @Mutation(() => Product)
+  async updateProduct(
+    @Args('productId') productId: string,
+    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+  ) {
+    // 인자값에 다 객체로 안묶고 따로 id만 빼서
+    // 전달하는 이유는 어떤 필드를 골라서 어떤 내용을 수정할지 명확하게 하기 위해서 임.
+
+    // 판매 완료가 되었는지 확인 로직
+    await this.productsService.checkSoldout({ productId });
+
+    // 수정하기: 수정 전 판매 가능 상태인지 판매 완료 검증 후 실행
+    return this.productsService.update({ productId, updateProductInput });
+  }
+
+  @Mutation(() => Boolean)
+  deleteProduct(
+    @Args('productId') productId: string, //
+  ) {
+    return this.productsService.delete({ productId });
+  }
+}
